b2b31eab9b47d786dd24f30b863132332bbd64cb
Sören Helmreich <soeren.helmreich@gmail.com>
2024-11-13 11:41:01 +0100

    added cat fact feature to index.html and updated main.js to fetch and display cat facts

--- code/src/main.js
+++ code/src/main.js
@@ -3,13 +3,38 @@ import {  getMinutesLeftofYear, getSecondsLeftOfYear, getTimeString } from "./cl
 const timeContainer = document.getElementById("time");
 const secondsLeft = document.getElementById("seconds-left");
 const minutesLeft = document.getElementById("minutes-left");
+const catFact = document.getElementById("cat-fact");
 
-const render = () => {
+
+async function fetchCatFact(){
+  try {
+
+    const response = await fetch("https://catfact.ninja/fact");
+
+    const object = await response.json();
+
+    console.log(`Server response: '${JSON.stringify(object.fact)}'`);
+    
+    return JSON.stringify(object.fact);
+  } catch (e) {
+    console.log("Error" + e.message)    
+  }
+}
+
+
+
+
+const render = async () => {
   const now = new Date();
   secondsLeft.innerText = getSecondsLeftOfYear(now);
   minutesLeft.innerText = getMinutesLeftofYear(now);
   timeContainer.innerText = getTimeString(now);
+  catFact.innerHTML = await fetchCatFact();
 }
 
 render();
-setInterval(render, 1000);
+setInterval(render, 10000);
+
+
+
+
